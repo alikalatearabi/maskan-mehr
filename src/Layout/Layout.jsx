208065d2fc253e7a2d79c63e6ed2a97d479c7ce5
alikalatearabi <ali.kalate89@hotmail.com>
2023-12-25 19:04:54 +0330

    add download help pdf to menu

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,7 +1,7 @@
-import React, { Fragment, useContext } from 'react';
-import { Outlet, useLocation } from 'react-router-dom';
-import { TransitionGroup, CSSTransition } from 'react-transition-group';
-import { ToastContainer } from 'react-toastify';
+import React, {Fragment, useContext} from 'react';
+import {Outlet, useLocation} from 'react-router-dom';
+import {TransitionGroup, CSSTransition} from 'react-transition-group';
+import {ToastContainer} from 'react-toastify';
 import Footer from './Footer/index';
 import Loader from './Loader';
 import TapTop from './TapTop/index';
@@ -12,58 +12,81 @@ import CheckContext from '../_helper/customizer';
 import ConfigDB from '../Config/Theme-Config';
 import AnimationThemeContext from '../_helper/AnimationTheme';
 
-const Layout = ({ children, classNames, ...rest }) => {
-  const { sidebar_types, settings, toggleIcon, setToggleIcon, defaultClass, setDefaultClass
-  } = useContext(CheckContext);
-  const settings1 = localStorage.getItem('sidebar_Settings') || settings;
-  const sidebar_types1 = localStorage.getItem('sidebar_types') || sidebar_types;
-  const location = useLocation();
-  const { animation } = useContext(AnimationThemeContext);
-  const animationTheme = localStorage.getItem('animation') || animation || ConfigDB.data.router_animation;
-  window.addEventListener('resize', () => {
-    if ((window.innerWidth - 440) <= 575) {
-      setToggleIcon(true);
-    } else {
-      setToggleIcon(false);
-    }
-    if (window.innerWidth <= 992) {
-      setDefaultClass(true);
-    } else setDefaultClass(false);
-  });
-  return (
-    <Fragment>
-      <Loader />
-      <TapTop />
-      <div className={`page-wrapper ${!defaultClass ? sidebar_types1 : 'compact-wrapper'} ${settings1}`} id="pageWrapper">
-        <div className={`page-header ${toggleIcon ? 'close_icon' : ''}`}>
-          <Header />
-        </div>
-        <div className="page-body-wrapper">
-          <div className={`sidebar-wrapper ${toggleIcon ? 'close_icon' : ''}`}>
-            <div>
-              <SideBarLayout />
-            </div>
-          </div>
-          <div className="page-body">
-            <TransitionGroup {...rest}>
-              <CSSTransition
-                key={location.key}
-                timeout={100}
-                classNames={animationTheme}
-                unmountOnExit
-              >
-                <div>
-                  <Outlet />
+const Layout = ({children, classNames, ...rest}) => {
+    const {
+        sidebar_types, settings, toggleIcon, setToggleIcon, defaultClass, setDefaultClass
+    } = useContext(CheckContext);
+    const settings1 = localStorage.getItem('sidebar_Settings') || settings;
+    const sidebar_types1 = localStorage.getItem('sidebar_types') || sidebar_types;
+    const location = useLocation();
+    const {animation} = useContext(AnimationThemeContext);
+    const animationTheme = localStorage.getItem('animation') || animation || ConfigDB.data.router_animation;
+    window.addEventListener('resize', () => {
+        if ((window.innerWidth - 440) <= 575) {
+            setToggleIcon(true);
+        } else {
+            setToggleIcon(false);
+        }
+        if (window.innerWidth <= 992) {
+            setDefaultClass(true);
+        } else setDefaultClass(false);
+    });
+    return (
+        <Fragment>
+            <Loader/>
+            <TapTop/>
+            <div className={`page-wrapper ${!defaultClass ? sidebar_types1 : 'compact-wrapper'} ${settings1}`}
+                 id="pageWrapper">
+                <div className={`page-header ${toggleIcon ? 'close_icon' : ''}`}>
+                    <Header/>
                 </div>
-              </CSSTransition>
-            </TransitionGroup>
-          </div>
-          <Footer />
-        </div>
-      </div>
-      <Themecustomizer />
-      <ToastContainer rtl />
-    </Fragment>
-  );
+                <div className="page-body-wrapper">
+                    <div className={`sidebar-wrapper ${toggleIcon ? 'close_icon' : ''}`}>
+                        <SideBarLayout/>
+                        <a
+                            target={'_blank'}
+                            style={{
+                                background: '#cacaca',
+                                position: 'absolute',
+                                bottom: '10px',
+                                left: '5px',
+                                right: '5px',
+                                height: '40px',
+                                borderRadius: '10px',
+                                padding: '10px',
+                                display: 'flex',
+                                alignItems: 'center',
+                                justifyContent: 'center',
+                                fontSize: '20px',
+                                cursor: 'pointer',
+                                boxShadow: '0 1px 2px grey',
+                                color: '#464646'
+                            }}
+                            href={'https://dl.maskanins.ir/files/Mehr%20Manual.pdf'}
+                        >
+                            دانلود راهنما
+                        </a>
+                    </div>
+                    <div className="page-body">
+                        <TransitionGroup {...rest}>
+                            <CSSTransition
+                                key={location.key}
+                                timeout={100}
+                                classNames={animationTheme}
+                                unmountOnExit
+                            >
+                                <div>
+                                    <Outlet/>
+                                </div>
+                            </CSSTransition>
+                        </TransitionGroup>
+                    </div>
+                    <Footer/>
+                </div>
+            </div>
+            <Themecustomizer/>
+            <ToastContainer rtl/>
+        </Fragment>
+    );
 };
 export default Layout;
